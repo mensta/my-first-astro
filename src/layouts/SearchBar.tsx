6b3675aebdb3ce64eb296aad83b773d088d73938
LostMyCode <63048878+LostMyCode@users.noreply.github.com>
2024-08-09 01:15:12 +0000

    fix for traling_slash f4578f6a5572b960494f7887f85fb628b21591db

--- src/layouts/SearchBar.tsx
+++ src/layouts/SearchBar.tsx
@@ -95,7 +95,7 @@ export default function SearchBar({ searchList }: Props) {
           <div key={item.slug} className={"col-12 mb-8 sm:col-6"}>
             {item.data.image && (
               <a
-                href={`/${item.slug}`}
+                href={`/${item.slug}/`}
                 className="rounded-lg block hover:text-primary overflow-hidden group"
               >
                 <img
@@ -135,7 +135,7 @@ export default function SearchBar({ searchList }: Props) {
 
             <h3 className="mb-2">
               <a
-                href={`/${item.slug}`}
+                href={`/${item.slug}/`}
                 className="block hover:text-primary transition duration-300"
               >
                 {item.data.title}
