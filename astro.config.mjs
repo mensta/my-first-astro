253e0d9fbe51aa26f1f06b967ec34a5f53a6a114
mensta <gee@sigr.io>
2024-12-04 09:17:06 +0000

    add config param: disable_image_optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
-  image: {
+  image: config.settings.disable_image_optimization ? undefined : {
     service: squooshImageService(),
   },
   integrations: [
